3b88c14bff92a430c05dcf41f01d9846fea2204b
Milosz Wielondek <miloszw@kth.se>
2017-03-13 00:52:11 +0100

    Add makeRequest
    Currently violates the same-origin policy. I might rewrite this
    as a node.js app instead. For now, disable Cross-Origin restrictions
    in your browser for this to work.

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 CONSUMER_KEY = "64614-1a557559fbd5ba7f0c1f79e5";
+REDIRECT = "http://local123host:8080/"
+
+var makeRequest = function(type, url, callback, params) {
+  function reqListener() {
+    console.log(this.getAllResponseHeaders());
+    var data = JSON.parse(this.responseText);
+    callback(data);
+  }
+
+  function reqError(err) {
+    console.log('Fetch Error :-S', err);
+    console.log(this.getAllResponseHeaders());
+  }
+
+  var oReq = new XMLHttpRequest();
+  oReq.onload = reqListener;
+  oReq.onerror = reqError;
+
+  oReq.open(type, url, true);
+  oReq.setRequestHeader('Content-Type', 'application/json; charset=UTF8');
+  oReq.setRequestHeader('X-Accept', 'application/json');
+  oReq.send(JSON.stringify(params));
+}
 
 window.onload = function() {
   document.getElementById('auth-init').onclick = function() {
     // init auth here -- see https://getpocket.com/developer/docs/authentication
+    makeRequest('POST', 'https://getpocket.com/v3/oauth/request', function(data) {
+      console.log(data);
+    }, {
+      'consumer_key': CONSUMER_KEY,
+      'redirect_uri': REDIRECT
+    });
   }
 }
