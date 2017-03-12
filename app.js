75b366aeb3b70bbc04cfb01ef02c17382df4c511
Milosz Wielondek <miloszw@kth.se>
2017-03-12 23:31:00 +0100

    Add consumer key

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+CONSUMER_KEY = "64614-1a557559fbd5ba7f0c1f79e5";
+
 window.onload = function() {
   document.getElementById('auth-init').onclick = function() {
     // init auth here -- see https://getpocket.com/developer/docs/authentication
