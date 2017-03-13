334555d570664b9c9960dc36faa0844de7da3587
Milosz Wielondek <miloszw@kth.se>
2017-03-13 01:16:57 +0100

    Rewrite app.js for node

--- app.js
+++ app.js
@@ -1,36 +1,33 @@
 CONSUMER_KEY = "64614-1a557559fbd5ba7f0c1f79e5";
-REDIRECT = "http://local123host:8080/"
+REDIRECT = "http://localhost:8080/"
+
+const request = require('request');
 
 var makeRequest = function(type, url, callback, params) {
-  function reqListener() {
-    console.log(this.getAllResponseHeaders());
-    var data = JSON.parse(this.responseText);
-    callback(data);
+  options = {
+    url: url,
+    method: type,
+    headers: {
+      'Content-Type': 'application/json; charset=UTF8',
+      'X-Accept': 'application/json'
+    },
+    body: JSON.stringify(params)
   }
 
-  function reqError(err) {
-    console.log('Fetch Error :-S', err);
-    console.log(this.getAllResponseHeaders());
-  }
-
-  var oReq = new XMLHttpRequest();
-  oReq.onload = reqListener;
-  oReq.onerror = reqError;
-
-  oReq.open(type, url, true);
-  oReq.setRequestHeader('Content-Type', 'application/json; charset=UTF8');
-  oReq.setRequestHeader('X-Accept', 'application/json');
-  oReq.send(JSON.stringify(params));
+  request(options, function(err, response, body) {
+    if (err) {
+      console.log("error: ", err)
+    } else {
+      callback(JSON.parse(body));
+    }
+  });
 }
 
-window.onload = function() {
-  document.getElementById('auth-init').onclick = function() {
-    // init auth here -- see https://getpocket.com/developer/docs/authentication
-    makeRequest('POST', 'https://getpocket.com/v3/oauth/request', function(data) {
-      console.log(data);
-    }, {
-      'consumer_key': CONSUMER_KEY,
-      'redirect_uri': REDIRECT
-    });
-  }
-}
+// init auth here -- see https://getpocket.com/developer/docs/authentication
+
+makeRequest('POST', 'https://getpocket.com/v3/oauth/request', function(data) {
+  console.log(data);
+}, {
+  'consumer_key': CONSUMER_KEY,
+  'redirect_uri': REDIRECT
+});
