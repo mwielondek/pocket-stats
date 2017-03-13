d0dc9b833ccf29605d17dff8c60aba25a8f13eb1
Milosz Wielondek <miloszw@kth.se>
2017-03-13 02:31:10 +0100

    Add whole OATH process

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 CONSUMER_KEY = "64614-1a557559fbd5ba7f0c1f79e5";
 REDIRECT = "http://localhost:8080/"
 
+const util = require('util');
 const request = require('request');
 
 var makeRequest = function(type, url, callback, params) {
@@ -25,9 +26,51 @@ var makeRequest = function(type, url, callback, params) {
 
 // init auth here -- see https://getpocket.com/developer/docs/authentication
 
-makeRequest('POST', 'https://getpocket.com/v3/oauth/request', function(data) {
-  console.log(data);
-}, {
-  'consumer_key': CONSUMER_KEY,
-  'redirect_uri': REDIRECT
-});
+function step1() {
+  console.log('* Step 1:');
+  makeRequest('POST', 'https://getpocket.com/v3/oauth/request', step2, {
+    'consumer_key': CONSUMER_KEY,
+    'redirect_uri': REDIRECT
+  });
+}
+
+function step2(data) {
+  var token = data.code;
+  console.log('Received code/token: ' + token);
+
+  console.log('* Step 2:');
+  var url = util.format("https://getpocket.com/auth/authorize?request_token=\
+%s&redirect_uri=%s",
+    token, REDIRECT);
+  console.log('Go to this url in your browser:\n' + url);
+
+  // SERVER PART listening for callback from pocket
+  const http = require('http');
+  const server = http.createServer(function(req, res) {
+    res.writeHead(200);
+    res.end('Go back to the app!');
+    step3(token);
+    server.close();
+  });
+  server.listen(8080);
+}
+
+function step3(token) {
+  console.log('* Step 3:');
+  console.log('Received callback from pocket - app is authorized.');
+  makeRequest('POST', 'https://getpocket.com/v3/oauth/authorize', step4, {
+    'consumer_key': CONSUMER_KEY,
+    'code': token
+  });
+}
+
+function step4(data) {
+  console.log('* Step 4:');
+  var access_token = data.access_token;
+  var username = data.username;
+  console.log('Retrieved access token for user ' + username);
+}
+
+
+
+// TODO break this up into utils (makeRequest), auth part, and query part
