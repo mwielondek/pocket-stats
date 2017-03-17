3bfb8e07dc7c75245923486f3fce84e4a503e8e4
Milosz Wielondek <miloszw@kth.se>
2017-03-17 13:39:09 +0100

    Init express app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+let express = require('express');
+let app = express();
+
+const PORT = 8080;
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, function () {
+  console.log(`Server listening on port ${PORT}....`);
+});
